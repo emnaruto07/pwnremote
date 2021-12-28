e984ebe2368612dfa0f04f78346331c4ff6739be
emnaruto07 <shazebvps@gmail.com>
2021-12-29 04:54:05 +0530

    x

--- pwnremote/jobs/admin.py
+++ pwnremote/jobs/admin.py
@@ -4,8 +4,6 @@ from .models import Job,CompanyDetail, Feedback, User, emailList
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Job)
-# admin.site.register(JobDetail)
-# admin.site.register(DesignPost)
 admin.site.register(CompanyDetail)
 admin.site.register(Feedback)
 admin.site.register(emailList)

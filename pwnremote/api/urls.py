514b83b77cefc78eeea7d1f40caee88efbab47af
emnaruto07 <shazebvps@gmail.com>
2021-12-29 21:56:00 +0530

    x

--- pwnremote/api/urls.py
+++ pwnremote/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import JobListView, JobCreateView, JobUpdateView, JobDeleteView, JobDetailView
 
 urlpatterns = [
-    path('jobs', JobListView.as_view()),
+    path('jobs/', JobListView.as_view()),
     path('create-job/', JobCreateView.as_view()),
     path('job/<pk>/update', JobUpdateView.as_view()),
     path('job/<pk>/delete', JobDeleteView.as_view()),

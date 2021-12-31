21671e29e01f3b5e507122b91c9abfb56f44ee77
emnaruto07 <shazebvps@gmail.com>
2022-01-01 03:55:44 +0530

    xx

--- pwnremote/pwnremote/urls.py
+++ pwnremote/pwnremote/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
-from dj_rest_auth.registration.views import VerifyEmailView
+from dj_rest_auth.registration.views import VerifyEmailView, ConfirmEmailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api-token-auth/', views.obtain_auth_token),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/account-confirm-email/<str:key>/',ConfirmEmailView.as_view()),
     path('dj-rest-auth/account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/', include('api.urls')),  

f583b6700952fae408f08398d433220cdf4040b6
emnaruto07 <shazebvps@gmail.com>
2022-01-10 11:26:47 +0530

    logo

--- frontend/src/components/JobList.js
+++ frontend/src/components/JobList.js
@@ -8,35 +8,35 @@ function JobListItem({job}){
   const condition = job.Highlight;
 
   return(  
+
     <NavLink to={`/jobs/${job.id}`}>
       <div {...condition ? 'true': "false" } style={{ backgroundColor: condition ? "#FAF884" : "white" }} className="border-solid border-2 px-2 py-3 shadow-lg rounded-2xl mb-3">
-        <div  className="flex justify-between">
-              <h4 className="mt-1">
-                <div className="inline">
-                  <div className="flex">
-                  {job.Company_name} <img src={New} alt="new" width="40" height="20" />
-                  </div> 
-                  <h3 className="text-xl text-black font-bold">{job.Position}</h3>
-                </div>     
-              </h4>
-              
-          {/* <div>
-              <a className="flex justify-between py-3" href={job.url} target="_blank" rel="noopener noreferrer">
-                <h5 className="border-solid border-2 border-black bg-black hover:text-black hover:bg-white text-white font-bold py-2 px-4 shadow-lg rounded-lg">Apply</h5>
-              </a>
-          </div> */}
-        </div>
-        <div className="flex">
-          <p>${job.Min_salary}-${job.max_salary}</p>   
-            {job.Location && (
-              <p className="font-semibold ml-2">
-                {job.Location}
-              </p>
-            )}     
-        </div>      
-          <div className="text-gray-600 text-sm">
-            {new Date(job.date_created).toDateString()}
-          </div>              
+          {job.company_logo && (
+              <img src={job.company_logo} className="h-20 w-20 rounded-full border-2" alt={job.company_logo} />
+          )}
+          <td className="p-2 object-center">
+            <div  className="flex justify-between">
+                  <div className="mt-1">
+                    <div className="inline">     
+                      <div className="flex">   
+                      {job.Company_name} <img src={New} alt="new" width="40" height="20" />
+                      </div> 
+                      <h3 className="text-xl text-black font-bold">{job.Position}</h3>
+                    </div>     
+                  </div>
+            </div>
+            <div className="flex">
+              <p>${job.Min_salary}-${job.max_salary}</p>   
+                {job.Location && (
+                  <p className="font-semibold ml-2">
+                    {job.Location}
+                  </p>
+                )}     
+            </div>      
+              <div className="text-gray-600 text-sm">
+                {new Date(job.date_created).toDateString()}
+              </div> 
+          </td>               
       </div>
     </ NavLink>
 

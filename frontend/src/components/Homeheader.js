1bd40e0cad82c00a43ef19d1099d2ba69f07a3ea
emnaruto07 <shazebvps@gmail.com>
2022-01-15 18:14:17 +0530

    x

--- frontend/src/components/Homeheader.js
+++ frontend/src/components/Homeheader.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import Typewriter from 'typewriter-effect';
+import Typewriter from 'typewriter-effect';
 import Banner from "../assets/banner.jpg";
 
 
@@ -12,7 +12,46 @@ function Homeheader() {
     //  }, []);
 
     return (
-        <div className="p-20 h-screen" style={{backgroundImage: `linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(${Banner})`}}>
+        <div className="py-20 aspect-w-4 aspect-h-8 bg-auto" style={{backgroundImage: `linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(${Banner})`}}>
+            <div className='flex justify-center items-center'>
+            <p className='text-red-500 font-bold text-4xl'>Apply</p>
+            <span className='items-center text-4xl ml-2 font-semibold text-white'>
+                <Typewriter
+                    onInit={(typewriter)=> {
+                    typewriter
+                    .typeString("WebApp Security")
+                    .deleteAll()
+                    .typeString("Researcher")
+                    .deleteAll()
+                    .typeString("SOC/SIEM")
+                    .deleteAll()
+                    .typeString("Pentesting")
+                    .deleteAll()
+                    .typeString("Consulting")
+                    .deleteAll()
+                    .typeString("Quality Assurance")
+                    .deleteAll()
+                    .typeString("Forensics")
+                    .deleteAll()
+                    .typeString("Mobile Security")
+                    .deleteAll()
+                    .typeString("Cloud Security")
+                    .deleteAll()
+                    .typeString("Infrastructure Security")
+                    .deleteAll()
+                    .typeString("Code Source Review")
+                    .deleteAll()
+                    .typeString("Hardware/IOT")
+                    .deleteAll()
+                    .typeString("DevOps/DevSecOps")
+                    .deleteAll()
+                    .typeString("Sales/Marketing")
+                    .deleteAll()
+                    .start();
+                    }}
+                    />
+            </span>
+            </div>
             
         </div>
         
